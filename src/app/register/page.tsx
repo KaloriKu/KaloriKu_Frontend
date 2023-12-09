003d2a768e5064a227d6ce70682ae88201d294fc
Airel Camilo Khairan <airel.camilo@ui.ac.id>
2023-12-09 09:53:35 +0700

    feat: revamp landing, register, & login page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,46 +2,112 @@
 
 import { useState } from 'react';
 import { useUserContext } from '@/modules/auth/UserContext';
+import { Box, Button, Center, FormControl, Heading, Text, Input } from "@chakra-ui/react";
+import { useForm } from 'react-hook-form';
+import { comfortaa } from '@/common/Style';
 
 const Register = () => {
-  const { register } = useUserContext();
-  const [nama, setNama] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const { handleSubmit, register } = useForm();
+  const { registerUser } = useUserContext();
+  const [isHover, setIsHover] = useState(false);
+  const [emailInvalid, setEmailInvalid] = useState(false);
+  const [passInvalid, setPassInvalid] = useState(false);
 
-  const handleRegister = async () => {
-    // TODO: Check field valid or not
-    register({
-      "email": email,
-      "password": password,
-      "nama": nama
+  const over = () => {
+    setIsHover(true);
+  }
+  const out = () => {
+    setIsHover(false);
+  }
+
+  const emailCheck = (e: any) => {
+    const email = e.target.value;
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+      setEmailInvalid(true);
+    } else {
+      setEmailInvalid(false);
+    }
+  }
+
+  const handleRegister = async (query: any) => {
+    if (emailInvalid || passInvalid) {
+      return
+    }
+
+    registerUser({
+      "email": query['email'],
+      "password": query['password'],
+      "nama": query['nama']
     })
   };
 
   return (
-    <div style={{'padding': '80px'}}>
-      <h1>Register</h1>
-      <form>
-        <label>
-          Nama:
-          <input style={{"color": "black"}} type="nama" value={nama} onChange={(e) => setNama(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input style={{"color": "black"}} type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input style={{"color": "black"}} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </label>
-        <br />
-        <button type="button" onClick={handleRegister}>
-          Register
-        </button>
-      </form>
-    </div>
+    <Box py={'150px'}>
+      <Center>
+        <Box w='320px'>
+          <form onSubmit={handleSubmit(handleRegister)}>
+            <Text fontSize={'30px'} className={comfortaa.className} fontWeight={'bold'} mb='15px'>Register akun Anda</Text>
+            <FormControl>
+              <Text mb='5px' mt='10px'>Nama</Text>
+              <Input {...register('nama')}
+                id='nama'
+                name='nama'
+                w='100%'
+                focusBorderColor='orange.100'
+                _hover={{
+                  bg: "gray.100",
+                  transitionDuration: '0.2s',
+                  transitionTimingFunction: "ease-in-out"
+                }}
+                isRequired
+                onMouseOver={over}
+                onMouseOut={out}
+                onChange={emailCheck} />
+              <Text mb='5px' mt='10px'>Email</Text>
+              <Input {...register('email')}
+                id='email'
+                name='email'
+                w='100%'
+                focusBorderColor='orange.100'
+                _hover={{
+                  bg: "gray.100",
+                  transitionDuration: '0.2s',
+                  transitionTimingFunction: "ease-in-out"
+                }}
+                isRequired
+                onMouseOver={over}
+                onMouseOut={out}
+                onChange={emailCheck} />
+              {emailInvalid
+                ? <Text color='red' mt='5px'>Email salah</Text>
+                : ""
+              }
+              <Text mb='5px' mt='10px'>Password</Text>
+              <Input {...register('password')}
+                id='password'
+                name='password'
+                w='100%'
+                focusBorderColor='orange.100'
+                _hover={{
+                  bg: "gray.100",
+                  transitionDuration: '0.2s',
+                  transitionTimingFunction: "ease-in-out"
+                }}
+                isRequired
+                type='password'
+                isInvalid={passInvalid}
+                onMouseOver={over}
+                onMouseOut={out} />
+              {passInvalid
+                ? <Text color='red' mt='5px'>Password salah</Text>
+                : ""
+              }
+              <Button colorScheme='orange' type='submit' mt='20px'>Register</Button>
+            </FormControl>
+          </form>
+        </Box>
+      </Center >
+    </Box >
   );
 };
 
