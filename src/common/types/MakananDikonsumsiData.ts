ee64c3bbfd1a575c4155c09cbfa1cbad1ede55b3
monicaoktviona <monicaoktaviona2002@gmail.com>
2023-12-11 19:40:02 +0700

    fix: fix error in MakananDikonsumsiData

--- src/common/types/MakananDikonsumsiData.ts
+++ src/common/types/MakananDikonsumsiData.ts
@@ -1,6 +1,7 @@
 import { MakananData } from '@/common/types/MakananData';
 
 export interface MakananDikonsumsiData{
+    details: any;
     timeStamp: string;
     totalKalori: number;
     totalLemak: number;
