f8e76df7f4428d512115c279928268e821bfd8ed
monicaoktviona <monicaoktaviona2002@gmail.com>
2023-12-11 12:56:56 +0700

    feat: add frontend for makanan_dikonsumsi

--- src/common/Style.ts
+++ src/common/Style.ts
@@ -1,7 +1,8 @@
 import { Inter, Comfortaa } from 'next/font/google'
 
 export enum Color {
-    ONE='#ff9747'
+    ONE='#ff9747',
+    TWO='#ffc294'
 }
 export const inter = Inter({ subsets: ['latin'] })
 export const comfortaa = Comfortaa({ subsets: ['latin'], weight: ['300', '400', '500', '700']})
